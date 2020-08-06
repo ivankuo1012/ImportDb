a0d4f8b187996b5554ccc2b4defa6d53f21a6469
Ivan Kuo <ivankuo1012@gmail.com>
2020-08-06 17:04:12 +0800

    開始開發

---
+++ ImportDb/ImportDb/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Net.Http;
+using System.IO;
+using System.Net.Mail;
+using System.Security.Cryptography;
+
+namespace ImportDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string sEncrKey = "8F916052C251C0B325318B1F3F2737EF";
+            string sDataSource = System.Configuration.ConfigurationManager.AppSettings.Get("DataSource");
+            string sCatalog = System.Configuration.ConfigurationManager.AppSettings.Get("Catalog");
+            string sDbUser = System.Configuration.ConfigurationManager.AppSettings.Get("DbUser");
+            string sDbPassword = DecryptStr(System.Configuration.ConfigurationManager.AppSettings.Get("DbPassword"), sEncrKey);
+
+            string sPath = ".\\txt_files";
+            string sFileNameEmpList = "NET_HRIS_EMP";
+            string sFileNameUniList = "UNI_RES";
+
+            string consString = "data source=" + sDataSource + "; initial catalog = " + sCatalog + "; user id = " + sDbUser + "; password = " + sDbPassword + "";
+
+            
+
+            SqlConnection conn = new SqlConnection(consString);
+
+            //SqlConnection conn = new SqlConnection("data source=.\\SQLExpress; initial catalog = FUBON_DLP; user id = fubon_dlp; password = 1234");
+            conn.Open();
+            try
+            {
+                // Only get files that begin with the letter "c".C:\Users\ivank\OneDrive\source\repos\ImportDb\ImportDb\ImportDb\txt_files\
+               
+                string[] dirs = Directory.GetFiles(sPath, sFileNameEmpList+"*");
+                //Console.WriteLine("The number of files starting with c is {0}.", dirs.Length);
+                if(dirs == null)
+                {
+                    Console.WriteLine("no file");
+                }
+                else
+                {
+                    string csvPath = dirs[0];
+                    Console.WriteLine(Path.GetFileName(dirs[0]));
+                    string csvData = System.IO.File.ReadAllText(csvPath);
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+
+                            Console.WriteLine(row);
+                            int i = 0;
+                            foreach (string cell in row.Split(','))
+                            {
+                                //dt.Rows[dt.Rows.Count - 1][i] = cell;
+                                //Response.Write(cell);
+                                i++;
+                            }
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+
+        }
+         public static string DecryptStr(string SourceStr, string CryptoKey)
+        {
+            string decrypt = "";
+            try
+            {
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                aes.Key = key;
+                aes.IV = iv;
+
+                byte[] dataByteArray = Convert.FromBase64String(SourceStr);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(dataByteArray, 0, dataByteArray.Length);
+                        cs.FlushFinalBlock();
+                        decrypt = Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return decrypt;
+        }
+    }
+}
